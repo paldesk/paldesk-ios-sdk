f622392e7e73bac58c0b55e4181e6b5713e2c4ad
Filip Matijević <fmatijevic@Filips-MacBook-Pro-2.local>
2018-05-23 15:55:36 +0200

    First version
    First version of Paldesk iOs sdk

---
+++ Paldesk.framework/Headers/Paldesk.h
@@ -0,0 +1,19 @@
+//
+//  Paldesk
+//  Paldesk
+//
+//  Created by Filip Matijevic on 27/12/2017.
+//  Copyright © 2017 Paldesk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Paldesk.
+FOUNDATION_EXPORT double paldesksdkVersionNumber;
+
+//! Project version string for Paldesk.
+FOUNDATION_EXPORT const unsigned char paldesksdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <paldesksdk/PublicHeader.h>
+
+
